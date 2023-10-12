d7a57403c3a8cd62037efc61f1926d07e1b3fe78
Andres Peña <afpena44@gmail.com>
2023-10-12 12:03:36 -0500

    se agrega firebase_core y firebase_auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:borrador_soli/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseUIAuth.configureProviders([
+    GoogleProvider(clientId: ""),
+  ]);
 }
 
 class MainApp extends StatelessWidget {
